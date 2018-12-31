3c3b62a45d1432786913eb1fc9219218c6f3b8bb
Bryan Harper <brharper@corelogic.com>
2018-12-31 14:49:25 -0600

    Get java running.

--- java/src/main/java/com/example/demo/BlankCanvas.java
+++ java/src/main/java/com/example/demo/BlankCanvas.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-public class BlankCanvast {
+public class BlankCanvas {
 
     void fooBar() {
         
--- java/src/main/java/com/example/demo/SomeLists.java
+++ java/src/main/java/com/example/demo/SomeLists.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SomeLists {
 
     List<String> listo;
