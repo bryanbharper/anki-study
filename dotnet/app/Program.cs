d291ce4d7ca59af2ac9fa85bc989e24e516bf58a
Bryan Harper <brharper@corelogic.com>
2018-12-31 11:51:33 -0600

    Handle program file changes.

--- dotnet/app/Program.cs
+++ dotnet/app/Program.cs
@@ -1,25 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using app.models;
 
 namespace app
 {
     class Program
     {
-        delegate void Delgo(string s);
-
-        /***********************************************************
-        ***********************************************************/
         static void Main(string[] args)
         {
-            object varbo = new object();
-            var dictionary = new Dictionary<int, string>
-            {
-                {1, "hi"},
-                {2, "bye"}
-            };
-
-
+            Console.WriteLine("Hello World!");
         }
     }
 }
