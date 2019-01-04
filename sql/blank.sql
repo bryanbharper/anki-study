65ffcd13368c4e1a65f8d95a929b5f4e57e23294
Bryan Harper <brharper@corelogic.com>
2019-01-04 15:12:57 -0600

    Add column to Colleciton.

--- sql/blank.sql
+++ sql/blank.sql
@@ -1,4 +1,4 @@
 CREATE table A (Col INTEGER);
 CREATE table B (Col INTEGER);
-CREATE table Collection (Col1 INTEGER, Col2 INTEGER);
+CREATE table Collection (Col1 INTEGER, Col2 INTEGER, Col TEXT);
 
