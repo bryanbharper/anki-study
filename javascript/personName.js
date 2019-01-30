1a0e1c84a2a7eb5946e2e15aefdfd388b535d41e
Bryan Harper <brharper@corelogic.com>
2019-01-30 12:03:36 -0600

    Add file for map funciton.

---
+++ javascript/personName.js
@@ -0,0 +1,19 @@
+class FullName {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+    }
+}
+
+class Person {
+    constructor(first, last, age) {
+        this.firstName = first;
+        this.lastName = last;
+        this.age = age;
+    }
+}
+
+
+let people = [new Person("Bob", "Smith", 48), new Person("Jill", "Bluthe", 35)];
+
+// Solution:
