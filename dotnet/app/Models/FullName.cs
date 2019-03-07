38a0ce18eb58b6834c5c0b6c078e6f944a82264b
Bryan Harper <brharper@corelogic.com>
2019-03-06 18:05:40 -0600

    Add FullName constructor.

--- dotnet/app/Models/FullName.cs
+++ dotnet/app/Models/FullName.cs
@@ -2,6 +2,12 @@ namespace app.models
 {
     public class FullName
     {
+
+        public FullName(string first, string second)
+        {
+            this.First = first;
+            this.Last = Last;
+        }
         public string First { get; set; }
         public string Last { get; set; }
     }
