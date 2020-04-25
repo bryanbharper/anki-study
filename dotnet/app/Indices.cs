f4d0657497453c786524c68400f5ac5652bdf171
Bryan Harper <brharper@corelogic.com>
2020-04-25 15:10:22 -0500

    Indices

---
+++ dotnet/app/Indices.cs
@@ -0,0 +1,13 @@
+namespace anki.indices
+{
+	class Indices
+	{
+		string[] words = new string[] { "The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog", };
+
+		public void Foobar()
+		{
+			// Solution here:
+			
+		}
+	}
+}
