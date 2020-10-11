5b2430c67fe94d023b7602dcb0a5c5c529ae4132
Bryan Harper <brharper@corelogic.com>
2020-10-11 10:47:36 -0500

    Some misc F#

--- fsharp/List.fsx
+++ fsharp/List.fsx
@@ -1,4 +1,5 @@
-let list1 = [2; 3; 4]
-let list2 = [5; 6; 7]
-let list3 = [1; 1; 1; 2; 2; 3]
-
+let list1 = [ 2; 3; 4 ]
+let list2 = [ 5; 6; 7 ]
+let list3 = [ 1; 1; 1; 2; 2; 3 ]
+let array1 = [| 1; 2; 3 |]
+let asyncList = list1 |> List.map async.Return
