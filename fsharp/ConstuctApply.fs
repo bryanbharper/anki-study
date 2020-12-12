f2b94883d1ee01e8d0feb0be33d96ad06423ba39
Bryan Harper <brharper@corelogic.com>
2020-12-12 16:10:03 -0600

    Construct apply.

---
+++ fsharp/ConstuctApply.fs
@@ -0,0 +1,17 @@
+namespace Construct.Apply
+
+type E<'a> = 
+    | E of 'a
+
+module E =
+    let rtn x = E x
+
+    let map (f: 'a -> 'b) (e: E<'a>): E<'b> = 
+        let (E a) = e
+        a |> f |> rtn
+
+    let bind (binder: 'a -> E<'b>) (e: E<'a>): E<'b> =
+        let (E a) = e
+        binder a
+
+    // let apply (ef: E<'a -> 'b>) (e: E<'a>): E<'b> =
